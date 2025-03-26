9165bfc771f526f523fb07466701625bcd60a700
mike <hongyufei0709@outlook.com>
2025-03-26 16:50:26 -0400

    添加数字图片

--- ImageToArray.java
+++ ImageToArray.java
@@ -13,53 +13,59 @@ public class ImageToArray {
             System.out.println("path/to/ur/image");
             return;
         }
-
-        String entry = args[0];
+        int n = args.length;
+        for (int i = 0; i < n; i++) {
+            String entry = args[i];
         
-        String imagePath = "/Users/yufeihong/Desktop/ece243/243Project/utility/" + entry;
-        String name = entry.substring(0, entry.lastIndexOf("."));
-        try {
-            BufferedImage image = ImageIO.read(new File(imagePath));
+            String imagePath = "/Users/yufeihong/Desktop/ece243/243Project/utility/numbers/" + entry;
+            String name = entry.substring(0, entry.lastIndexOf("."));
+            try {
+                BufferedImage image = ImageIO.read(new File(imagePath));
 
-            int width = image.getWidth();
-            int height = image.getHeight();
-            System.out.println("size : " + width + " * " + height + "\n");
-             if (width > 320 || height > 240) {
-                 System.out.println("图片过大");
-                 return;
-             }
-            FileWriter writer = new FileWriter("/Users/yufeihong/Desktop/ece243/243Project/utility/" + name + ".h");
+                int width = image.getWidth();
+                int height = image.getHeight();
+                System.out.println("size : " + width + " * " + height + "\n");
+                 if (width > 320 || height > 240) {
+                     System.out.println("图片过大");
+                     return;
+                 }
+                FileWriter writer = new FileWriter("/Users/yufeihong/Desktop/ece243/243Project/utility/numbers/" + name + ".h");
 
-            writer.write("#ifndef " + name + "_H\n#define " + name + "_H\n\n");
-            writer.write("const unsigned short " + name + "[240][320] = {\n");
+                writer.write("#ifndef " + name + "_H\n#define " + name + "_H\n\n");
+                writer.write("const unsigned short " + name + "[240][320] = {\n");
 
-            for (int y = 0; y < 240; y++) {
-                writer.write("    {");
-                for (int x = 0; x < 320; x++) {
-                    if (x >= width || y >= height) {
-                        writer.write(String.format("0x0000"));
+                for (int y = 0; y < 240; y++) {
+                    writer.write("    {");
+                    for (int x = 0; x < 320; x++) {
+                        if (x >= width || y >= height) {
+                            writer.write(String.format("0x0000"));
+                            if (x < 320 - 1) writer.write(", ");
+                            continue;
+                        }
+                        int rgb = image.getRGB(x, y);
+                        int red = (rgb >> 16) & 0xFF;
+                        int green = (rgb >> 8) & 0xFF;
+                        int blue = rgb & 0xFF;
+                        int threshold_white = 200;
+                        int threshold_black = 50;
+                        int pixel565 = ((red >> 3) << 11) | ((green >> 2) << 5) | (blue >> 3);
+                        if (red >= threshold_white && (green >= threshold_white && blue >= threshold_white)) {
+                            pixel565 = 0xffff;
+                        }
+                        if (red <= threshold_black && green <= threshold_black && blue <= threshold_black) {
+                            pixel565 = 0x0000;
+                        }
+                        writer.write(String.format("0x%04X", pixel565));
                         if (x < 320 - 1) writer.write(", ");
-                        continue;
                     }
-                    int rgb = image.getRGB(x, y);
-                    int red = (rgb >> 16) & 0xFF;
-                    int green = (rgb >> 8) & 0xFF;
-                    int blue = rgb & 0xFF;
-                    int threshold = 200;
-                    int pixel565 = ((red >> 3) << 11) | ((green >> 2) << 5) | (blue >> 3);
-                    if (red >= threshold && (green >= threshold && blue >= threshold)) {
-                        pixel565 = 0xffff;
-                    }
-                    writer.write(String.format("0x%04X", pixel565));
-                    if (x < 320 - 1) writer.write(", ");
+                    writer.write("},\n");
                 }
-                writer.write("},\n");
-            }
-            writer.write("};\n\n#endif // "+ name + "_H\n");
-            writer.close();
+                writer.write("};\n\n#endif // "+ name + "_H\n");
+                writer.close();
 
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
--- converter.sh
+++ converter.sh
@@ -1,25 +1,22 @@
 #!/bin/zsh
-if [[ $# -ne 1 ]]; then
+if [[ $# -lt 1 ]]; then
     echo "路径不对"
     echo "./convert path/to/image.jpg"
     exit 1
 fi
 
-IMAGE_PATH="$1"
-# if [[ ! -f "$IMAGE_PATH" ]]; then
-#     echo "图片\"$IMAGE_PATH\" 不存在"
-#     exit 1
-# fi
+for IMAGE_PATH in "$@"; do
 
-javac ImageToArray.java
-if [[ $? -ne 0 ]]; then
-    echo "编译失败"
-    exit 1
-fi
+    javac ImageToArray.java
+    if [[ $? -ne 0 ]]; then
+        echo "编译失败"
+        exit 1
+    fi
 
-java ImageToArray "$IMAGE_PATH"
-if [[ $? -eq 0 ]]; then
-    echo "ok了👌"
-else
-    echo "转换不成功"
-fi
+    java ImageToArray "$IMAGE_PATH"
+    if [[ $? -eq 0 ]]; then
+        echo "ok了👌"
+    else
+        echo "转换不成功"
+    fi
+done
